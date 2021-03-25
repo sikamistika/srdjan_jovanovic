b0f7a3fa58cc89d01b1ea45bab1399f1270b1a0c
sikamistika <srdjan.jovanovic73@gmail.com>
2021-03-25 20:38:12 +0100

    25-03-21

---
+++ bit-pp/20.03.2021/test7.js
@@ -0,0 +1,7 @@
+// Write a program that intertwines two arrays. You can assume the arrays are of the same
+// length.
+// Input arrays: [4, 5, 6, 2], [3, 8, 11, 9]
+// Output array: [4, 3, 5, 8, 6, 11, 2, 9]
+
+
+var a = [4, 5, 6, 2], [3, 8, 11, 9]
---
+++ bit-pp/20.03.2021/test8.js
@@ -0,0 +1,3 @@
+// Write a program that concatenates two arrays.
+// Input arrays: [4, 5, 6, 2], [3, 8, 11, 9]
+// Output array: [4, 5, 6, 2, 3, 8, 11, 9]
