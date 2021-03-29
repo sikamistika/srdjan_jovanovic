b3912e9a28c2fbb7ecfe8950ad58d9ab64242b9d
sikamistika <srdjan.jovanovic73@gmail.com>
2021-03-30 00:23:56 +0200

    vezbanje 29-03-21

---
+++ bit-pp/Vezbanje/funkcije.js
@@ -0,0 +1,95 @@
+// function comp (x, y) {
+//     if (x>y){
+//     return x;
+//      } else
+//     return y;
+// }
+//     result = comp (5, 14)
+//    console.log(result)
+
+// function odd (x){
+//     if(x%2 !== 0){
+//         return x + ' is odd'
+//     }else
+//     return x + ' is even'
+// }
+//     result = odd (14)
+//     console.log(result)
+
+// function threeDigitsNumber (x){
+// if (x>99 && x<=999){
+//     return 'Number '+ x + ' is three digit number'
+// }else
+//     return 'Number ' + x+ ' is not three digit number'
+// }
+//     result = threeDigitsNumber(8)
+//     console.log(result)
+
+// function aritMean(a, b, c, d){
+//     var e = (a+b+c+d)/4
+//     return e
+
+// }
+//     result = aritMean(5, 5, 5, 5)
+//     console.log(result)
+
+
+//ZADATAK IZ NIZOVA
+
+// var a = [1, 2, 3, 4]
+// var b = [5, 6, 7, 8]
+
+// var newArrey = []
+
+// for(i=0; i<a.length; i++){
+//       newArrey[newArrey.length]=a[i]
+//       newArrey[newArrey.length]=b[i]
+// }
+
+//     console.log(newArrey)
+
+
+// var a =[1, 8, 9, 5, 4, 6, 9, 2, 7]
+
+// function zbirNegativnih(a) { 
+//     var zbir = 0
+//     for(i=0; i<a.length; i++){
+//         if (a[i] !== 0){
+//             zbir = zbir += a[i]
+//          }
+     
+//     }return zbir
+// }   
+//     result = zbirNegativnih(a)
+//     console.log(result)
+
+
+// function horChart(x, y, z) {
+// var chart = ''
+// for(i=0; i<x; i++){
+//     var chart = chart += '*'
+// }
+// chart += '\n'
+
+// for (i=0; i<y; i++){
+//     var chart = chart += '*'
+// }
+//     chart += '\n'
+
+// for (i=0; i<z; i++){
+//     var chart = chart += '*'
+// }
+// chart += '\n'
+
+// return chart
+
+
+// }
+// result = horChart(10, 15, 6)
+// console.log(result)
+
+
+
+
+
+
