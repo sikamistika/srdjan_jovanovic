9b0156711c89479e16759e1658ba3d4a6133edcc
sikamistika <srdjan.jovanovic73@gmail.com>
2021-05-31 13:29:31 +0200

    29.05.2021

---
+++ bit-pp/29.05.2021/task1.js
@@ -0,0 +1,11 @@
+var user = {
+    userrname: 'pera',
+    password: '12345',
+    email: 'pera@MediaList.com',
+    age: 25,
+    isAdmin: false
+};
+
+
+console.log(user);
+// console.log(typeof user);
